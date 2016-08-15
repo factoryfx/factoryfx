278f2fc9e6381376d4e50564fa0de1a1878bb18f
hbrackmann <henning.brackmann@scoop-software.de>
2016-08-15 16:29:07 +0200

    findbugs

--- factory/src/test/java/de/factoryfx/factory/datastorage/filesystem/FileSystemFactoryStorageTest.java
+++ factory/src/test/java/de/factoryfx/factory/datastorage/filesystem/FileSystemFactoryStorageTest.java
@@ -3,6 +3,7 @@ package de.factoryfx.factory.datastorage.filesystem;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import de.factoryfx.factory.testfactories.ExampleFactoryA;
 import org.junit.Assert;
@@ -34,6 +35,20 @@ public class FileSystemFactoryStorageTest {
         FileSystemFactoryStorage<ExampleFactoryA> restored = new FileSystemFactoryStorage<>(Paths.get(folder.getRoot().toURI()),null,ExampleFactoryA.class);
         restored.loadInitialFactory();
         Assert.assertEquals(id,restored.getCurrentFactory().metadata.id);
+    }
+
+    @Test
+    public void test_update() throws MalformedURLException {
+        FileSystemFactoryStorage<ExampleFactoryA> fileSystemFactoryStorage = new FileSystemFactoryStorage<>(Paths.get(folder.getRoot().toURI()), new ExampleFactoryA(),ExampleFactoryA.class);
+        fileSystemFactoryStorage.loadInitialFactory();
+        String id=fileSystemFactoryStorage.getCurrentFactory().metadata.id;
+
+
+        fileSystemFactoryStorage.updateCurrentFactory(new ExampleFactoryA(),"fhdgd");
+        Assert.assertNotEquals(id,fileSystemFactoryStorage.getCurrentFactory().metadata.id);
+        Assert.assertEquals(1,fileSystemFactoryStorage.getHistoryFactoryList().size());
+        Assert.assertEquals(id,new ArrayList<>(fileSystemFactoryStorage.getHistoryFactoryList()).get(0).id);
+
     }
 }
 
