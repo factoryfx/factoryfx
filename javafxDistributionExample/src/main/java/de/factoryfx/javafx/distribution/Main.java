aca63312e26afb22945b74c361c3e442c9f30b2f
hbrackmann <henning.brackmann@scoop-software.de>
2016-10-07 16:16:47 +0200

    factorysation

--- javafxDistributionExample/src/main/java/de/factoryfx/javafx/distribution/Main.java
+++ javafxDistributionExample/src/main/java/de/factoryfx/javafx/distribution/Main.java
@@ -3,10 +3,9 @@ package de.factoryfx.javafx.distribution;
 import java.io.File;
 
 import de.factoryfx.adminui.SinglePrecessInstanceUtil;
-import de.factoryfx.javafx.distribution.server.GuiFileService;
+import de.factoryfx.javafx.distribution.launcher.ui.UserInterfaceDistributionClient;
 import de.factoryfx.javafx.distribution.server.UserInterfaceDistributionServer;
 import de.factoryfx.javafx.distribution.server.rest.DownloadResource;
-import de.factoryfx.javafx.distribution.launcher.ui.UserInterfaceDistributionClient;
 import javafx.application.Application;
 
 public class Main {
@@ -15,7 +14,7 @@ public class Main {
         SinglePrecessInstanceUtil.enforceSingleProzessInstance(12344);
 
         int port = 13678;
-        UserInterfaceDistributionServer server = new UserInterfaceDistributionServer("localhost", port,new DownloadResource(new GuiFileService(new File("./build/distributions/UserInterface-0.1.0_0.zip"))));
+        UserInterfaceDistributionServer server = new UserInterfaceDistributionServer("localhost", port,new DownloadResource(new File("./build/distributions/UserInterface-0.1.0_0.zip")));
         new Thread(){
             @Override
             public void run() {
