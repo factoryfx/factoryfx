3796a50c73b6a7174aa61fb098dd2db53369cf40
Roland Scheel <roland.scheel@scoop-software.de>
2016-10-25 13:35:59 +0200

    add helper method to convert to final list

--- data/src/main/java/de/factoryfx/data/attribute/types/StringListAttribute.java
+++ data/src/main/java/de/factoryfx/data/attribute/types/StringListAttribute.java
@@ -5,6 +5,10 @@ import de.factoryfx.data.attribute.AttributeMetadata;
 import de.factoryfx.data.attribute.ValueListAttribute;
 import de.factoryfx.data.jackson.ObservableListJacksonAbleWrapper;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringListAttribute extends ValueListAttribute<String> {
 
     public StringListAttribute(AttributeMetadata attributeMetadata) {
@@ -16,4 +20,9 @@ public class StringListAttribute extends ValueListAttribute<String> {
         super(null,null,null);
         set(list.unwrap());
     }
+
+    public List<String> asUnmodifiableList() {
+        return Collections.unmodifiableList(get().stream().collect(Collectors.toList()));
+    }
+
 }
