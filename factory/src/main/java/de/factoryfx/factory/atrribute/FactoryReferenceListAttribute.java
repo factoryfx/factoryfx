40841b3f0369e6a724927aecbc8467686e52385a
Roland Scheel <roland.scheel@scoop-software.de>
2016-10-25 10:14:33 +0200

    fix npe

--- factory/src/main/java/de/factoryfx/factory/atrribute/FactoryReferenceListAttribute.java
+++ factory/src/main/java/de/factoryfx/factory/atrribute/FactoryReferenceListAttribute.java
@@ -2,8 +2,12 @@ package de.factoryfx.factory.atrribute;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import de.factoryfx.data.Data;
 import de.factoryfx.data.attribute.AttributeMetadata;
 import de.factoryfx.data.attribute.ReferenceListAttribute;
 import de.factoryfx.data.jackson.ObservableListJacksonAbleWrapper;
@@ -45,4 +49,17 @@ public class FactoryReferenceListAttribute<L,T extends FactoryBase<L,?>> extends
     public boolean add(T data){
         return get().add(data);
     }
+
+    /**customise the list of selectable items*/
+    public FactoryReferenceListAttribute possibleValueProvider(Function<Data,List<T>> provider){
+        super.possibleValueProvider(provider);
+        return this;
+    }
+
+    /**customise how new values are created*/
+    public FactoryReferenceListAttribute newValueProvider(Supplier<T> newValueProvider){
+        super.newValueProvider(newValueProvider);
+        return this;
+    }
+
 }
