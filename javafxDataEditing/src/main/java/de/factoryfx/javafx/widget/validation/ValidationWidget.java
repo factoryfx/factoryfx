16b208e4eed83f61e1dea6905b04a25ae1c80104
hbrackmann <henning.brackmann@scoop-software.de>
2017-02-15 11:13:54 +0100

    improved ValidationWidget

--- javafxDataEditing/src/main/java/de/factoryfx/javafx/widget/validation/ValidationWidget.java
+++ javafxDataEditing/src/main/java/de/factoryfx/javafx/widget/validation/ValidationWidget.java
@@ -1,5 +1,7 @@
 package de.factoryfx.javafx.widget.validation;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import de.factoryfx.data.Data;
@@ -18,8 +20,9 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -37,6 +40,7 @@ public class ValidationWidget implements Widget {
     private final Data root;
     private final UniformDesign uniformDesign;
     private final DataEditor dataEditor;
+
     public ValidationWidget(Data root, DataEditor dataEditor, UniformDesign uniformDesign){
         this.root = root;
         this.uniformDesign = uniformDesign;
@@ -46,10 +50,12 @@ public class ValidationWidget implements Widget {
     private static class ValidationAndData{
         public final ValidationError validationError;
         public final Data data;
+        public final String dataDisplayText;
 
-        public ValidationAndData(ValidationError validationError, Data data) {
+        public ValidationAndData(ValidationError validationError, Data data, String dataDisplayText) {
             this.validationError = validationError;
             this.data = data;
+            this.dataDisplayText = dataDisplayText;
         }
     }
 
@@ -57,28 +63,41 @@ public class ValidationWidget implements Widget {
     public Node createContent() {
         final SplitPane splitPane = new SplitPane();
         splitPane.setOrientation(Orientation.VERTICAL);
-        final TableView<ValidationAndData> tableView = new TableView<>();
-        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        final TreeTableView<ValidationAndData> tableView = new TreeTableView<>();
+        tableView.setColumnResizePolicy(TreeTableView.CONSTRAINED_RESIZE_POLICY);
         {
-            final TableColumn<ValidationAndData, String> column = new TableColumn<>(uniformDesign.getText(columnData));
-            column.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().data.internal().getDisplayText()));
+            final TreeTableColumn<ValidationAndData, String> column = new TreeTableColumn<>(uniformDesign.getText(columnData));
+            column.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().dataDisplayText));
             tableView.getColumns().add(column);
         }
         {
-            final TableColumn<ValidationAndData, String> column = new TableColumn<>(uniformDesign.getText(columnField));
-            column.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().validationError.attributeDescription(uniformDesign::getText)));
+            final TreeTableColumn<ValidationAndData, String> column = new TreeTableColumn<>(uniformDesign.getText(columnField));
+            column.setCellValueFactory(param -> {
+                String initialValue = "";
+                if (param.getValue().getValue().validationError!=null){
+                    initialValue = param.getValue().getValue().validationError.attributeDescription(uniformDesign::getText);
+                }
+                return new SimpleStringProperty(initialValue);
+            });
             tableView.getColumns().add(column);
         }
         {
-            final TableColumn<ValidationAndData, String> column = new TableColumn<>(uniformDesign.getText(columnValidation));
-            column.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().validationError.validationDescription(uniformDesign::getText)));
+            final TreeTableColumn<ValidationAndData, String> column = new TreeTableColumn<>(uniformDesign.getText(columnValidation));
+            column.setCellValueFactory(param -> {
+                        String initialValue = "";
+                        if (param.getValue().getValue().validationError!=null){
+                            initialValue = param.getValue().getValue().validationError.validationDescription(uniformDesign::getText);
+                        }
+                        return new SimpleStringProperty(initialValue);
+            });
             tableView.getColumns().add(column);
         }
 
+
         tableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue!=null){
-                dataEditor.edit(newValue.data);
-                dataEditor.setHistory(newValue.data.internal().getPathFromRoot());
+                dataEditor.edit(newValue.getValue().data);
+                dataEditor.setHistory(newValue.getValue().data.internal().getPathFromRoot());
             } else {
                 dataEditor.edit(null);
                 dataEditor.resetHistory();
@@ -97,6 +116,7 @@ public class ValidationWidget implements Widget {
         vBox.getChildren().add(buttons);
 
         validate(tableView);
+        tableView.getRoot().setExpanded(true);
 
 //        SplitPane.setResizableWithParent(vBox, Boolean.TRUE);
         splitPane.getItems().add(vBox);
@@ -119,22 +139,38 @@ public class ValidationWidget implements Widget {
         return borderPane;
     }
 
-    private void validate(TableView<ValidationAndData> tableView){
+    private void validate(TreeTableView<ValidationAndData> tableView){
         Data selected= dataEditor.editData().get();
-        tableView.getItems().clear();
-        for (Data data: root.internal().collectChildrenDeep()){
-            tableView.getItems().addAll(data.internal().validateFlat().stream().map(validationError->new ValidationAndData(validationError,data)).collect(Collectors.toList()));
+        final TreeItem<ValidationAndData> root = new TreeItem<>();
+        tableView.setRoot(root);
+        tableView.setShowRoot(false);
+
+        List<ValidationError> validationErrors= new ArrayList<>();
+        for (Data data: this.root.internal().collectChildrenDeep()){
+            TreeItem<ValidationAndData> dataItem = new TreeItem<>();
+            dataItem.setExpanded(true);
+            dataItem.setValue(new ValidationAndData(null,data,data.internal().getPathFromRoot().stream().map(d->d.internal().getDisplayText()).collect(Collectors.joining("/"))));
+            data.internal().validateFlat().forEach(validationError->{
+                validationErrors.add(validationError);
+                TreeItem<ValidationAndData> error =new TreeItem<>(new ValidationAndData(validationError,data,data.internal().getDisplayText()));
+                dataItem.getChildren().add(error);
+            });
+            if (!dataItem.getChildren().isEmpty()){
+                root.getChildren().add(dataItem);
+            }
         }
         tableView.getStyleClass().remove("error");
-        if (!tableView.getItems().isEmpty()){
+        if (!validationErrors.isEmpty()){
             tableView.getStyleClass().add("error");
         }
-        tableView.getItems().forEach(validationAndData -> {
-            if (validationAndData.data==selected){
-                tableView.getSelectionModel().select(validationAndData);
-            }
-        });
-        isValid.set(tableView.getItems().isEmpty());
+        tableView.getSelectionModel().clearSelection();
+        dataEditor.reset();
+//        validationErrors.forEach(validationAndData -> {
+//            if (validationAndData.data==selected){
+//                tableView.getSelectionModel().select(validationAndData);
+//            }
+//        });
+        isValid.set(validationErrors.isEmpty());
     }
 
     SimpleBooleanProperty isValid=new SimpleBooleanProperty();
