b1eeb7db8f113939c9aa52bf7b77bc82145a6c52
hbrackmann <henning.brackmann@scoop-software.de>
2017-04-05 14:53:05 +0200

    server same path

--- jettyFactory/src/main/java/de/factoryfx/server/rest/server/JettyServer.java
+++ jettyFactory/src/main/java/de/factoryfx/server/rest/server/JettyServer.java
@@ -6,7 +6,6 @@ import java.util.function.Function;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import de.factoryfx.data.jackson.ObjectMapperBuilder;
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.ErrorHandler;
@@ -25,13 +24,18 @@ public class JettyServer {
         server=new org.eclipse.jetty.server.Server();
         connectors.forEach(serverServerConnectorFunction -> server.addConnector(serverServerConnectorFunction.apply(server)));
 
-        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
-        resources.forEach(jerseyResource -> contextHandler.addServlet( new ServletHolder(new ServletContainer(jerseySetup(jerseyResource))), "/*"));
-
+        HandlerCollection handlers = new HandlerCollection();
         ErrorHandler errorHandler = new ErrorHandler();
         errorHandler.setShowStacks(true);
-        contextHandler.setErrorHandler(errorHandler);
+        handlers.addHandler(errorHandler);
+
+        resources.forEach(jerseyResource -> {
+            final ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+            handlers.addHandler(contextHandler);
+            contextHandler.addServlet( new ServletHolder(new ServletContainer(jerseySetup(jerseyResource))), "/*");
+        });
+
 
         GzipHandler gzipHandler = new GzipHandler();
 //            HashSet<String> mimeTypes = new HashSet<>();
@@ -42,8 +46,7 @@ public class JettyServer {
 //            mimeTypes.add("application/json");
         gzipHandler.setMinGzipSize(0);
 
-        HandlerCollection handlers = new HandlerCollection();
-        handlers.setHandlers(new Handler[]{contextHandler, contextHandler});
+
         gzipHandler.setHandler(handlers);
         server.setHandler(gzipHandler);
     }
