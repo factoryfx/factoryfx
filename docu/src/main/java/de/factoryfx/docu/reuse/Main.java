71c98b70bc3b4235fdf0b4b1c26abd312da8af5f
hbrackmann <henning.brackmann@scoop-software.de>
2017-05-19 12:54:40 +0200

    docu examples

--- docu/src/main/java/de/factoryfx/docu/reuse/Main.java
+++ docu/src/main/java/de/factoryfx/docu/reuse/Main.java
@@ -1,18 +1,34 @@
 package de.factoryfx.docu.reuse;
 
 import de.factoryfx.factory.FactoryManager;
+import de.factoryfx.factory.datastorage.FactoryAndNewMetadata;
 import de.factoryfx.factory.datastorage.inmemory.InMemoryFactoryStorage;
 import de.factoryfx.factory.exception.RethrowingFactoryExceptionHandler;
 import de.factoryfx.server.ApplicationServer;
 
+import java.util.function.Function;
+
+/** example how to reuse a resource that is expensive to create usually that could be something like a connection pool or socket connection  */
 public class Main {
 
     public static void main(String[] args) {
         RootFactory root = new RootFactory();
+        root.stringAttribute.set("1");
 
+        long start=System.currentTimeMillis();
         ApplicationServer<Void,Root,RootFactory> applicationServer = new ApplicationServer<>(new FactoryManager<>(new RethrowingFactoryExceptionHandler<>()),new InMemoryFactoryStorage<>(root));
         applicationServer.start();
-        applicationServer.stop();
+
+        //over 5000ms most time for the ExpensiveResource
+        System.out.println(System.currentTimeMillis()-start);
+
+        long updateStart=System.currentTimeMillis();
+        FactoryAndNewMetadata<RootFactory> update = applicationServer.prepareNewFactory();
+        update.root.stringAttribute.set("2");
+        applicationServer.updateCurrentFactory(update, "", "", s -> true);
+
+        //much less than the 5000ms => ExpensiveResource not recreated
+        System.out.println(System.currentTimeMillis()-updateStart);
 
     }
 }
--- docu/src/main/java/de/factoryfx/docu/reuse/RootFactory.java
+++ docu/src/main/java/de/factoryfx/docu/reuse/RootFactory.java
@@ -1,8 +1,11 @@
 package de.factoryfx.docu.reuse;
 
+import de.factoryfx.data.attribute.AttributeMetadata;
+import de.factoryfx.data.attribute.types.StringAttribute;
 import de.factoryfx.factory.FactoryBase;
 
 public class RootFactory extends FactoryBase<Root,Void> {
+    public final StringAttribute stringAttribute = new StringAttribute(new AttributeMetadata());
 
     public RootFactory(){
         configLiveCycle().setCreator(() ->  new Root(new ExpensiveResource()));
