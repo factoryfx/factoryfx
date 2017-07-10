4e2a9c3ecab21b21228e88798a2aa7bf80f84fc4
hbrackmann <henning.brackmann@scoop-software.de>
2017-07-10 15:40:18 +0200

    small api extensions

--- factory/src/main/java/de/factoryfx/factory/parametrized/ParametrizedObjectCreatorAttribute.java
+++ factory/src/main/java/de/factoryfx/factory/parametrized/ParametrizedObjectCreatorAttribute.java
@@ -1,7 +1,12 @@
 package de.factoryfx.factory.parametrized;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import de.factoryfx.data.Data;
+import de.factoryfx.data.validation.Validation;
 import de.factoryfx.factory.atrribute.FactoryReferenceAttribute;
+
+import java.util.Collection;
+import java.util.function.Function;
 
 public class ParametrizedObjectCreatorAttribute<P, L, T extends ParametrizedObjectCreatorFactory<P,L,?>> extends FactoryReferenceAttribute<ParametrizedObjectCreator<P,L>,T> {
 
@@ -24,4 +29,33 @@ public class ParametrizedObjectCreatorAttribute<P, L, T extends ParametrizedObje
         return instance().create(p);
     }
 
+    @Override
+    public ParametrizedObjectCreatorAttribute<P, L, T> labelText(String text) {
+        super.labelText(text);
+        return this;
+    }
+
+    @Override
+    public ParametrizedObjectCreatorAttribute<P, L, T> de(String text) {
+        super.de(text);
+        return this;
+    }
+
+    @Override
+    public ParametrizedObjectCreatorAttribute<P, L, T> en(String text) {
+        super.en(text);
+        return this;
+    }
+
+    @Override
+    public ParametrizedObjectCreatorAttribute<P, L, T> validation(Validation<T> validation) {
+        super.validation(validation);
+        return this;
+    }
+
+    @Override
+    public ParametrizedObjectCreatorAttribute<P, L, T> possibleValueProvider(Function<Data, Collection<T>> provider) {
+        super.possibleValueProvider(provider);
+        return this;
+    }
 }
