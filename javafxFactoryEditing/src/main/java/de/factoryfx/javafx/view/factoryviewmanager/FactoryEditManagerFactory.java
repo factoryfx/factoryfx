45c46f40a72a124c27334cdb88cdb54f387c6d84
hbrackmann <henning.brackmann@scoop-software.de>
2017-08-30 11:42:11 +0200

    fixed generics for client

--- javafxFactoryEditing/src/main/java/de/factoryfx/javafx/view/factoryviewmanager/FactoryEditManagerFactory.java
+++ javafxFactoryEditing/src/main/java/de/factoryfx/javafx/view/factoryviewmanager/FactoryEditManagerFactory.java
@@ -10,18 +10,20 @@ import de.factoryfx.server.rest.client.ApplicationServerRestClientFactory;
 
 /**
  *
- * @param <V> server visitor
- * @param <R> server root
+ * @param <V> client visitor
+ * @param <R> client root
+ * @param <VS> server visitor
+ * @param <RS> server root
  */
-public class FactoryEditManagerFactory<V,R  extends FactoryBase<?,V>> extends SimpleFactoryBase<FactoryEditManager<V,R>,Void> {
+public class FactoryEditManagerFactory<V,R  extends FactoryBase<?,V>,VS,RS  extends FactoryBase<?,VS>> extends SimpleFactoryBase<FactoryEditManager<VS,RS>,Void> {
 
-    public final FactoryReferenceAttribute<ApplicationServerRestClient<V,R>,ApplicationServerRestClientFactory<V,R>> restClient = new FactoryReferenceAttribute<ApplicationServerRestClient<V,R>,ApplicationServerRestClientFactory<V,R>>().setupUnsafe(ApplicationServerRestClientFactory.class).de("restClient").en("restClient");
+    public final FactoryReferenceAttribute<ApplicationServerRestClient<VS,RS>,ApplicationServerRestClientFactory<V,R,VS,RS>> restClient = new FactoryReferenceAttribute<ApplicationServerRestClient<VS,RS>,ApplicationServerRestClientFactory<V,R,VS,RS>>().setupUnsafe(ApplicationServerRestClientFactory.class);
     //TODO refactor to FactoryReferenceAttribute?
-    public final ObjectValueAttribute<FactorySerialisationManager<R>> factorySerialisationManager = new ObjectValueAttribute<FactorySerialisationManager<R>>().de("factorySerialisationManager").en("factorySerialisationManager");
+    public final ObjectValueAttribute<FactorySerialisationManager<RS>> factorySerialisationManager = new ObjectValueAttribute<>();
 
 
     @Override
-    public FactoryEditManager<V,R> createImpl() {
+    public FactoryEditManager<VS,RS> createImpl() {
         return new FactoryEditManager<>(restClient.instance(),factorySerialisationManager.get());
     }
 
