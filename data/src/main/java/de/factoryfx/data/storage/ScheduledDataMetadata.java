a724491566ee43e98cd4ebbf804f94d72965e337
hbrackmann <henning.brackmann@scoop-software.de>
2017-12-11 13:48:44 +0100

    future deployment ready for DelayQueue

--- data/src/main/java/de/factoryfx/data/storage/ScheduledDataMetadata.java
+++ data/src/main/java/de/factoryfx/data/storage/ScheduledDataMetadata.java
@@ -1,15 +1,20 @@
 package de.factoryfx.data.storage;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
 
 /**
  * metadata for a future data
  */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public class ScheduledDataMetadata extends StoredDataMetadata {
+public class ScheduledDataMetadata extends StoredDataMetadata implements Delayed{
 
     /** for scheduled update: date and time of planned activation of this configuration */
     public LocalDateTime scheduled;
@@ -24,5 +29,42 @@ public class ScheduledDataMetadata extends StoredDataMetadata {
         this.baseVersionId=storedDataMetadata.baseVersionId;
         this.dataModelVersion=storedDataMetadata.dataModelVersion;
         this.scheduled=scheduled;
+    }
+
+    @JsonIgnore
+    @Override
+    public long getDelay(TimeUnit unit) {
+        return convert(unit).between(LocalDateTime.now(),scheduled);
+    }
+
+    @JsonIgnore
+    @Override
+    public int compareTo(Delayed o) {
+        return scheduled.compareTo(((ScheduledDataMetadata)o).scheduled);
+    }
+
+
+    private ChronoUnit convert(TimeUnit unit) {
+        if (unit == null) {
+            return null;
+        }
+        switch (unit) {
+            case DAYS:
+                return ChronoUnit.DAYS;
+            case HOURS:
+                return ChronoUnit.HOURS;
+            case MINUTES:
+                return ChronoUnit.MINUTES;
+            case SECONDS:
+                return ChronoUnit.SECONDS;
+            case MICROSECONDS:
+                return ChronoUnit.MICROS;
+            case MILLISECONDS:
+                return ChronoUnit.MILLIS;
+            case NANOSECONDS:
+                return ChronoUnit.NANOS;
+            default:
+                return null;
+        }
     }
 }
--- data/src/test/java/de/factoryfx/data/storage/ScheduledDataMetadataTest.java
+++ data/src/test/java/de/factoryfx/data/storage/ScheduledDataMetadataTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
@@ -19,4 +21,64 @@ public class ScheduledDataMetadataTest {
 
     }
 
+    @Test
+    public void test_delay(){
+
+        ScheduledDataMetadata scheduledDataMetadata = new ScheduledDataMetadata();
+        scheduledDataMetadata.scheduled= LocalDateTime.of(2000,1,1,1,1);
+        Assert.assertTrue(scheduledDataMetadata.getDelay(TimeUnit.MILLISECONDS)<0);
+
+
+        scheduledDataMetadata.scheduled= LocalDateTime.of(2100,1,1,1,1);
+        Assert.assertTrue(scheduledDataMetadata.getDelay(TimeUnit.MILLISECONDS)>0);
+    }
+
+    @Test
+    public void test_compare(){
+
+        ScheduledDataMetadata scheduledDataMetadataOld = new ScheduledDataMetadata();
+        scheduledDataMetadataOld.scheduled= LocalDateTime.of(2000,1,1,1,1);
+
+        ScheduledDataMetadata scheduledDataMetadataNew = new ScheduledDataMetadata();
+        scheduledDataMetadataNew.scheduled= LocalDateTime.of(2100,1,1,1,1);
+
+        Assert.assertTrue(scheduledDataMetadataOld.compareTo(scheduledDataMetadataNew)<0);
+    }
+
+    @Test
+    public void test_DelayQueue_past(){
+        DelayQueue<ScheduledDataMetadata> queue = new DelayQueue<>();
+        ScheduledDataMetadata scheduledDataMetadata = new ScheduledDataMetadata();
+        scheduledDataMetadata.scheduled= LocalDateTime.of(2000,1,1,1,1);
+        queue.offer(scheduledDataMetadata);
+        Assert.assertEquals(1,queue.size());
+        Assert.assertNotNull(queue.poll());
+    }
+
+    @Test
+    public void test_DelayQueue_past_order(){
+        DelayQueue<ScheduledDataMetadata> queue = new DelayQueue<>();
+
+        ScheduledDataMetadata scheduledDataMetadata1 = new ScheduledDataMetadata();
+        scheduledDataMetadata1.scheduled= LocalDateTime.of(2000,1,1,1,1);
+        queue.offer(scheduledDataMetadata1);
+        ScheduledDataMetadata scheduledDataMetadata2 = new ScheduledDataMetadata();
+        scheduledDataMetadata2.scheduled= LocalDateTime.of(2000,1,1,1,2);
+        queue.offer(scheduledDataMetadata2);
+
+        Assert.assertEquals(2,queue.size());
+        Assert.assertEquals(scheduledDataMetadata1,queue.poll());
+        Assert.assertEquals(scheduledDataMetadata2,queue.poll());
+    }
+
+
+    @Test
+    public void test_DelayQueue_future(){
+        DelayQueue<ScheduledDataMetadata> queue = new DelayQueue<>();
+        ScheduledDataMetadata scheduledDataMetadata = new ScheduledDataMetadata();
+        scheduledDataMetadata.scheduled= LocalDateTime.of(2100,1,1,1,1);
+        queue.offer(scheduledDataMetadata);
+        Assert.assertEquals(1,queue.size());
+        Assert.assertNull(queue.poll());
+    }
 }
