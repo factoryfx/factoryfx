1b5272ad0d5e6c1b215fee0e2f215558e3b56919
Michael Havlik <michael.havlik@scoop-software.de>
2018-01-12 10:06:25 +0100

    add uniqueness checker for Lists

--- data/src/main/java/de/factoryfx/data/validation/UniqueList.java
+++ data/src/main/java/de/factoryfx/data/validation/UniqueList.java
@@ -1,22 +1,9 @@
 package de.factoryfx.data.validation;
 
-import java.util.HashSet;
-import java.util.List;
-
 import de.factoryfx.data.Data;
-import de.factoryfx.data.util.LanguageText;
 
-public class UniqueList<T extends Data> implements Validation<List<T>> {
-
-    @Override
-    public ValidationResult validate(List<T> list) {
-        boolean error=false;
-        HashSet<Object> set = new HashSet<>();
-        for (T item : list) {
-            if (!set.add(item.getId())) {
-                error = true;
-            }
-        }
-        return new ValidationResult(error,new LanguageText().en("List contains duplicates entries"));
+public class UniqueList<T extends Data> extends UniqueListBy<T, String> {
+    public UniqueList() {
+        super(Data::getId);
     }
 }
