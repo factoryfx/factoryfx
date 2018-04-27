740ec7ae9d51a7f12fe95bdd9fa6d3c0d2134253
Michael Havlik <michael.havlik@scoop-software.de>
2018-04-27 11:07:57 +0200

    remove duplicate folders:
    drwxr-xr-x 3 himself himself  4096 Apr 27 10:41 microServiceRestClient
    drwxr-xr-x 3 himself himself  4096 Apr 27 10:41 microServiceRestCommon
    drwxr-xr-x 3 himself himself  4096 Apr 27 10:41 microServiceRestIntegrationTest
    drwxr-xr-x 3 himself himself  4096 Apr 27 10:41 microServiceRestServer
    drwxr-xr-x 3 himself himself  4096 Apr 27 10:41 microserviceRestClient
    drwxr-xr-x 3 himself himself  4096 Apr 27 10:41 microserviceRestCommon
    drwxr-xr-x 3 himself himself  4096 Apr 27 10:41 microserviceRestIntegrationTest
    drwxr-xr-x 3 himself himself  4096 Apr 27 10:41 microserviceRestServer

--- microServiceRestCommon/src/main/java/de/factoryfx/server/rest/UpdateCurrentFactoryRequest.java
+++
@@ -1,8 +0,0 @@
-package de.factoryfx.server.rest;
-
-import de.factoryfx.data.storage.DataAndNewMetadata;
-
-public class UpdateCurrentFactoryRequest {
-    public DataAndNewMetadata factoryUpdate;
-    public String comment;
-}
