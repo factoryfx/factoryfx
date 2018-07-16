6be53846c3b61b21b8bc8bd7a7929af5046df67e
Michael Havlik <michael.havlik@scoop-software.de>
2018-07-16 09:15:14 +0200

    with asserts

--- factory/src/test/java/de/factoryfx/factory/FactoryReferenceListTest.java
+++ factory/src/test/java/de/factoryfx/factory/FactoryReferenceListTest.java
@@ -1,5 +1,7 @@
 package de.factoryfx.factory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.Assert;
@@ -14,23 +16,35 @@ import de.factoryfx.server.Microservice;
 
 public class FactoryReferenceListTest {
 
+    private static List<String> creator = new ArrayList<>();
+    private static List<String> recreator = new ArrayList<>();
+    private static List<String> starter = new ArrayList<>();
+    private static List<String> destroyer = new ArrayList<>();
+
     public static class ObjectFactory extends FactoryBase<Object, Void, RootFactory> {
         ObjectFactory() {
             configLiveCycle().setCreator(() -> {
                 System.out.println("creator called " + getId());
+                FactoryReferenceListTest.creator.add(getId());
                 return new Object();
             });
             configLiveCycle().setReCreator(old -> {
                 System.out.println("reCreator called " + getId());
+                FactoryReferenceListTest.recreator.add(getId());
                 return new Object();
             });
-            configLiveCycle().setStarter(o -> System.out.println("starter called " + getId()));
-            configLiveCycle().setDestroyer(o -> System.out.println("destroyer called " + getId()));
+            configLiveCycle().setStarter(i -> {
+                System.out.println("starter called " + getId());
+                starter.add(getId());
+            });
+            configLiveCycle().setDestroyer(i -> {
+                System.out.println("destroyer called " + getId());
+                destroyer.add(getId());
+            });
         }
     }
 
     public static class RootFactory extends SimpleFactoryBase<String, Void, RootFactory> {
-
         public final FactoryReferenceListAttribute<Object, ObjectFactory> objects = new FactoryReferenceListAttribute<>(ObjectFactory.class);
 
         @Override
@@ -43,18 +57,32 @@ public class FactoryReferenceListTest {
     public void referenceListTest() {
 
         RootFactory rootFactory = new RootFactory();
-        rootFactory.objects.add(new ObjectFactory());
+        ObjectFactory first = new ObjectFactory();
+        rootFactory.objects.add(first);
         Microservice<Void, RootFactory, Void> microService = new Microservice<>(new FactoryManager<>(new RethrowingFactoryExceptionHandler()), new InMemoryDataStorage<>(rootFactory));
         microService.start();
 
-
         {
+            Assert.assertTrue(creator.contains(first.getId()));
+            creator.clear();
+            recreator.clear();
             System.out.println("update started");
             DataAndNewMetadata<RootFactory> update1 = microService.prepareNewFactory();
-            update1.root.objects.add(new ObjectFactory());
+            ObjectFactory second = new ObjectFactory();
+            update1.root.objects.add(second);
 
             FactoryUpdateLog<RootFactory> res1 = microService.updateCurrentFactory(update1, "root", "update1", s -> true);
             Assert.assertTrue(res1.successfullyMerged());
+
+            //  does first have to be created or recreated here?
+            // TODO activate:
+            // Assert.assertFalse(creator.contains(first.getId()));
+            // Assert.assertFalse(starter.contains(first.getId()));
+            // Assert.assertFalse(destroyer.contains(first.getId()));
+
+            Assert.assertFalse(recreator.contains(first.getId()));
+            Assert.assertTrue(creator.contains(second.getId()));
+            Assert.assertTrue(starter.contains(second.getId()));
         }
 
     }
