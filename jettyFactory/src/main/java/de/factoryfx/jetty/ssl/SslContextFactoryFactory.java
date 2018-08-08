c4ad0997c9a1e16679957592759f604680c9eb66
Michael Havlik <michael.havlik@scoop-software.de>
2018-08-08 16:37:55 +0200

    add Attributes for SslContextFactory

--- jettyFactory/src/main/java/de/factoryfx/jetty/ssl/SslContextFactoryFactory.java
+++ jettyFactory/src/main/java/de/factoryfx/jetty/ssl/SslContextFactoryFactory.java
@@ -6,28 +6,35 @@ import java.security.KeyStore;
 
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
+import de.factoryfx.data.attribute.primitive.BooleanAttribute;
 import de.factoryfx.data.attribute.types.Base64Attribute;
 import de.factoryfx.data.attribute.types.EnumAttribute;
 import de.factoryfx.data.attribute.types.StringAttribute;
 import de.factoryfx.factory.FactoryBase;
 import de.factoryfx.factory.SimpleFactoryBase;
 
-public class SslContextFactoryFactory<V,R extends FactoryBase<?,V,R>> extends SimpleFactoryBase<SslContextFactory,V, R> {
-    public final Base64Attribute keyStore=new Base64Attribute().en("keyStore").de("keyStore");
-    public final EnumAttribute<KeyStoreType> keyStoreType=new EnumAttribute<>(KeyStoreType.class);
+public class SslContextFactoryFactory<V, R extends FactoryBase<?, V, R>> extends SimpleFactoryBase<SslContextFactory, V, R> {
+    public final Base64Attribute keyStore = new Base64Attribute().en("keyStore").de("keyStore");
+    public final EnumAttribute<KeyStoreType> keyStoreType = new EnumAttribute<>(KeyStoreType.class);
     public final StringAttribute keyStorePassword = new StringAttribute().en("keyStorePassword").de("keyStorePassword");
+    public final StringAttribute keyPassword = new StringAttribute().en("keyPassword").de("keyPassword").nullable();
 
     public final Base64Attribute trustStore = new Base64Attribute().en("trustStore").de("trustStore");
-    public final EnumAttribute<KeyStoreType> trustStoreType=new EnumAttribute<>(KeyStoreType.class);
+    public final EnumAttribute<KeyStoreType> trustStoreType = new EnumAttribute<>(KeyStoreType.class);
     public final StringAttribute trustStorePassword = new StringAttribute().en("trustStorePassword").de("trustStorePassword");
 
     public final StringAttribute certAlias = new StringAttribute().en("certAlias").de("certAlias").nullable();
+
+    public final BooleanAttribute wantClientAuth = new BooleanAttribute().en("wantClientAuth").de("wantClientAuth").defaultValue(false);
+    public final BooleanAttribute needClientAuth = new BooleanAttribute().en("needClientAuth").de("needClientAuth").defaultValue(false);
 
     @Override
     public SslContextFactory createImpl() {
         SslContextFactory sslContextFactory = new SslContextFactory();
 
         sslContextFactory.setKeyStorePassword(keyStorePassword.get());
+        if (!keyPassword.isEmpty()) { sslContextFactory.setKeyManagerPassword(keyPassword.get()); }
+
         try {
             KeyStore keyStore = KeyStore.getInstance(keyStoreType.get().value());
             try (InputStream inputStream = new ByteArrayInputStream(this.keyStore.getBytes())) {
@@ -37,7 +44,8 @@ public class SslContextFactoryFactory<V,R extends FactoryBase<?,V,R>> extends Si
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
+        sslContextFactory.setNeedClientAuth(needClientAuth.get());
+        sslContextFactory.setWantClientAuth(wantClientAuth.get());
         sslContextFactory.setKeyStorePassword(trustStorePassword.get());
         try {
             KeyStore keyStore = KeyStore.getInstance(trustStoreType.get().value());
