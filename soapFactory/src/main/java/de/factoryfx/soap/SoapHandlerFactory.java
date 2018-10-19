08be31f6abe78043aa54eaab4a08b98d025f2c05
Michael Havlik <michael.havlik@scoop-software.de>
2018-10-19 09:35:51 +0200

    fix build error

--- soapFactory/src/main/java/de/factoryfx/soap/SoapHandlerFactory.java
+++ soapFactory/src/main/java/de/factoryfx/soap/SoapHandlerFactory.java
@@ -1,22 +1,23 @@
 package de.factoryfx.soap;
 
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import javax.servlet.http.HttpServletRequest;
+
 import de.factoryfx.factory.FactoryBase;
 import de.factoryfx.factory.SimpleFactoryBase;
 import de.factoryfx.factory.atrribute.FactoryReferenceAttribute;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
-import java.util.function.Predicate;
-
-public class SoapHandlerFactory<S,V,R extends FactoryBase<?,V,R>> extends SimpleFactoryBase<SoapHandler<S>,V,R> {
+public class SoapHandlerFactory<S,V,R extends FactoryBase<?,V,R>> extends SimpleFactoryBase<SoapHandler,V,R> {
 
     public final FactoryReferenceAttribute<S,FactoryBase<S,V,R>> serviceBean = new FactoryReferenceAttribute<>(null);
     public final FactoryReferenceAttribute<Predicate<HttpServletRequest>,FactoryBase<Predicate<HttpServletRequest>,V,R>> urlTest = new FactoryReferenceAttribute<>(null);
 
     @Override
-    public SoapHandler<S> createImpl() {
+    public SoapHandler createImpl() {
         S webService = serviceBean.instance();
-        return new SoapHandler<>(new WebServiceRequestDispatcher(webService), new SOAPMessageUtil(JAXBSoapUtil.getJAXBContextForWebService(webService.getClass())), Optional.ofNullable(urlTest.instance()).orElse(s->true));
+        return new SoapHandler(new WebServiceRequestDispatcher(webService), new SOAPMessageUtil(JAXBSoapUtil.getJAXBContextForWebService(webService.getClass())), Optional.ofNullable(urlTest.instance()).orElse(s->true));
     }
 }
 
