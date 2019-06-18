b504006d28f7a0c2fb34e475c8b970733058d0d0
hbrackmann <henning.brackmann@scoop-software.de>
2019-06-18 18:15:51 +0200

    webgui ++

--- jettyFactory/src/test/java/io/github/factoryfx/jetty/UpdateableServletTest.java
+++ jettyFactory/src/test/java/io/github/factoryfx/jetty/UpdateableServletTest.java
@@ -191,7 +191,7 @@ public class UpdateableServletTest {
                 UpdateableTestResourceFactory resource = new UpdateableTestResourceFactory();
                 resource.response.set("123");
 
-                ServletContextHandlerFactory<UpdateableWebserverRootFactory> servletContextHandler = (ServletContextHandlerFactory<UpdateableWebserverRootFactory>) ((GzipHandlerFactory<UpdateableWebserverRootFactory>) update.root.server.get().handler.get().handlers.get(0)).handler.get();
+                ServletContextHandlerFactory<UpdateableWebserverRootFactory> servletContextHandler = (ServletContextHandlerFactory<UpdateableWebserverRootFactory>) ( update.root.server.get().handler.get().handlers.get(0));
                 JerseyServletFactory<UpdateableWebserverRootFactory> jerseyServletFactory = new JerseyServletFactory<>();
                 jerseyServletFactory.restLogging.set(new Slf4LoggingFeatureFactory<>());
                 jerseyServletFactory.resources.add(resource);
