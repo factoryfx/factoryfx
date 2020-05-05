5f460d438f99d62f7d75a98de22d611facd1ab3b
Michael Havlik <michael.havlik@scoop-software.de>
2020-05-05 11:32:23 +0200

    extend example

--- docu/src/main/java/io/github/factoryfx/docu/update/RootFactory.java
+++ docu/src/main/java/io/github/factoryfx/docu/update/RootFactory.java
@@ -1,14 +1,13 @@
 package io.github.factoryfx.docu.update;
 
-import io.github.factoryfx.factory.attribute.types.StringAttribute;
 import io.github.factoryfx.factory.FactoryBase;
-
+import io.github.factoryfx.factory.attribute.types.StringAttribute;
 
 public class RootFactory extends FactoryBase<Root, RootFactory> {
     public final StringAttribute stringAttribute = new StringAttribute();
 
-    public RootFactory(){
-        configLifeCycle().setCreator(() ->  new Root(stringAttribute.get()));
+    public RootFactory() {
+        configLifeCycle().setCreator(() -> new Root(stringAttribute.get()));
         configLifeCycle().setStarter(root -> {
             //artificial long start time
             try {
@@ -18,5 +17,15 @@ public class RootFactory extends FactoryBase<Root, RootFactory> {
             }
         });
         configLifeCycle().setUpdater(root -> root.setDummy(stringAttribute.get()));
+
+        // You can also conditionally recreate the expensive resource
+        //configLifeCycle().setReCreator(root -> {
+        //    if (stringAttribute.get().equals("someCondition")) {
+        //        return new Root(stringAttribute.get());
+        //    } else {
+        //        root.setDummy(stringAttribute.get());
+        //        return root;
+        //    }
+        //});
     }
 }
