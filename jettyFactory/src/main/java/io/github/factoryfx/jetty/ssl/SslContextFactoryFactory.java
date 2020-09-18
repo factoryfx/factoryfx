cff6c0f0110c89ce192362ad556eab718adab803
Michael Havlik <michael.havlik@scoop-software.de>
2020-09-18 07:22:30 +0200

    Use new SslContextFactory constructors

--- jettyFactory/src/main/java/io/github/factoryfx/jetty/ssl/SslContextFactoryFactory.java
+++ jettyFactory/src/main/java/io/github/factoryfx/jetty/ssl/SslContextFactoryFactory.java
@@ -16,8 +16,10 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+import java.util.function.Supplier;
 
 public class SslContextFactoryFactory<R extends FactoryBase<?, R>> extends SimpleFactoryBase<SslContextFactory, R> {
+    public final EnumAttribute<ConnectorType> connectorType = new EnumAttribute<ConnectorType>().en("connectorType").de("connectorType");
     public final FileContentAttribute keyStore = new FileContentAttribute().en("keyStore").de("keyStore");
     public final EnumAttribute<KeyStoreType> keyStoreType = new EnumAttribute<KeyStoreType>().en("keyStoreType").de("keyStoreType");
     public final StringAttribute keyStorePassword = new StringAttribute().en("keyStorePassword").de("keyStorePassword");
@@ -34,10 +36,18 @@ public class SslContextFactoryFactory<R extends FactoryBase<?, R>> extends Simpl
     public final BooleanAttribute wantClientAuth = new BooleanAttribute().en("wantClientAuth").de("wantClientAuth").defaultValue(false);
     public final BooleanAttribute needClientAuth = new BooleanAttribute().en("needClientAuth").de("needClientAuth").defaultValue(false);
 
+    public enum ConnectorType {
+        SERVER(SslContextFactory.Server::new),
+        CLIENT(SslContextFactory.Client::new);
+        private final Supplier<SslContextFactory> sslContextFactorySupplier;
+
+        ConnectorType(Supplier<SslContextFactory> sslContextFactorySupplier) {this.sslContextFactorySupplier = sslContextFactorySupplier;}
+    }
+
     @Override
     protected SslContextFactory createImpl() {
         try {
-            SslContextFactory sslContextFactory = new SslContextFactory();
+            SslContextFactory sslContextFactory = connectorType.get().sslContextFactorySupplier.get();
 
             if (allowCipherSuites.size() > 0) {
                 sslContextFactory.setExcludeCipherSuites();
