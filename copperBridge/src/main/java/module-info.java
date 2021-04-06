415c53da2cd1173ccb6208c83cba22816a363c19
hbrackmann <henning.brackmann@scoop-software.de>
2021-04-06 19:27:39 +0200

    gradle upgrade

--- copperBridge/src/main/java/module-info.java
+++ copperBridge/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module io.github.factoryfx.copperBridge {
     requires transitive io.github.factoryfx.factory;
     requires org.copperengine.core;
     requires org.copperengine.ext;
+    requires org.yaml.snakeyaml;
 
     opens io.github.factoryfx.copperbridge;
     opens io.github.factoryfx.copperbridge.db;
