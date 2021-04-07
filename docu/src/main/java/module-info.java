07bae9f71c43308b11e1391b2d137f7a397274fa
hbrackmann <henning.brackmann@scoop-software.de>
2021-04-07 12:02:02 +0200

    gradle upgrade

--- docu/src/main/java/module-info.java
+++ docu/src/main/java/module-info.java
@@ -1,19 +1,19 @@
 open module io.github.factoryfx.docu {
     requires io.github.factoryfx.factory;
     requires java.ws.rs;
-    requires metrics.core;
+    requires com.codahale.metrics;
     requires io.github.factoryfx.jettyFactory;
-    requires metrics.jetty9;
-    requires jersey.common;
+    requires com.codahale.metrics.jetty9;
+//    requires jersey.common;
     requires com.google.common;
     requires ch.qos.logback.classic;
 
-    requires postgresql;
-    requires postgresql.embedded;
+//    requires postgresql;
+//    requires postgresql.embedded;
     requires java.naming;
 
     requires io.github.factoryfx.postgresqlStorage;
-    requires jersey.client;
+//    requires jersey.client;
     requires com.fasterxml.jackson.jaxrs.json;
 
     requires com.fasterxml.jackson.core;
@@ -21,10 +21,12 @@ open module io.github.factoryfx.docu {
     requires java.net.http;
     requires io.github.factoryfx.microserviceRestServer;
     requires io.github.factoryfx.microserviceRestClient;
-    requires javax.servlet.api;
+//    requires javax.servlet.api;
     requires org.eclipse.jetty.server;
     requires io.github.factoryfx.starter;
     requires io.github.factoryfx.domFactoryEditing;
     requires java.desktop;
+    requires org.postgresql.jdbc;
+    requires postgresql.embedded;
 
 }
