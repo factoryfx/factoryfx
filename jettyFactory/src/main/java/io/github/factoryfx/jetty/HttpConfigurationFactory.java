fc2494bad9948fddd5e3068f9e2f3eb945f06b64
Michael Havlik <michael.havlik@scoop-software.de>
2023-01-30 08:53:59 +0100

    add possibility to disable SNI host check

--- jettyFactory/src/main/java/io/github/factoryfx/jetty/HttpConfigurationFactory.java
+++ jettyFactory/src/main/java/io/github/factoryfx/jetty/HttpConfigurationFactory.java
@@ -1,6 +1,7 @@
 package io.github.factoryfx.jetty;
 
 import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
 
 import io.github.factoryfx.factory.FactoryBase;
 import io.github.factoryfx.factory.SimpleFactoryBase;
@@ -8,10 +9,16 @@ import io.github.factoryfx.factory.attribute.primitive.BooleanAttribute;
 
 public class HttpConfigurationFactory<R extends FactoryBase<?,R>> extends SimpleFactoryBase<HttpConfiguration,R> {
     public final BooleanAttribute sendServerVersion = new BooleanAttribute().de("sendServerVersion").en("sendServerVersion");
+    public final BooleanAttribute disableSniHostcheck = new BooleanAttribute().de("disable SNI host check").en("disable SNI host check");
 
     @Override
     protected HttpConfiguration createImpl() {
         HttpConfiguration httpConfiguration = new HttpConfiguration();
+        if(disableSniHostcheck.get()) {
+            SecureRequestCustomizer src = new SecureRequestCustomizer();
+            src.setSniHostCheck(false);
+            httpConfiguration.addCustomizer(src);
+        }
         httpConfiguration.setSendServerVersion(sendServerVersion.get());
         return httpConfiguration;
     }
